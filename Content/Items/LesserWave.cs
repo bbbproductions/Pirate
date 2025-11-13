e6928d91a2ceed8516e6b950e0906ee244d5f252
bbbproductions <bbbproductions0@gmail.com>
2025-11-13 13:44:30 -0800

    Lesser wave added

---
+++ Content/Items/LesserWave.cs
@@ -0,0 +1,58 @@
+using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Pirate.Content.Projectiles;
+
+namespace Pirate.Content.Items
+{
+    public class LesserWave : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.width = 28;
+            Item.height = 30;
+            Item.useStyle = ItemUseStyleID.HoldUp; 
+            Item.useTime = 30;
+            Item.useAnimation = 30;
+            Item.damage = 18;                     
+            Item.knockBack = 6f;                  
+            Item.DamageType = DamageClass.Magic;
+            Item.mana = 8;                        
+            Item.noMelee = true;
+            Item.value = Item.buyPrice(silver: 1);
+            Item.rare = ItemRarityID.Blue;
+            Item.UseSound = SoundID.Item21;       
+            Item.autoReuse = true;
+            Item.shoot = ModContent.ProjectileType<Projectiles.LesserWaveProjectile>();
+            Item.shootSpeed = 10f;
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient(ItemID.Book, 1)
+                .AddIngredient(ItemID.BucketWater, 1)
+                .AddTile(TileID.Bookcases)
+                .Register();
+        }
+
+         public override void ModifyTooltips(List<TooltipLine> tooltips)
+		{
+			var lineToChange = tooltips.FirstOrDefault(x => x.Name == "Damage" && x.Mod == "Terraria");
+			if(lineToChange != null)
+			{
+				string[] split = lineToChange.Text.Split(' ');
+				lineToChange.Text = split.First() + " pirate " + split.Last();
+			}
+		}
+
+		public override void ModifyWeaponDamage(Player player, ref StatModifier damage)
+		{
+			damage += player.GetModPlayer<GlobalPlayer>().pirateDamage;
+		}
+    }
+}
---
+++ Content/Projectiles/LesserWaveProjectile.cs
@@ -0,0 +1,40 @@
+using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+
+namespace Pirate.Content.Projectiles
+{
+    public class LesserWaveProjectile : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Lesser Wave");
+        }
+
+        public override void SetDefaults()
+        {
+            Projectile.width = 24;
+            Projectile.height = 16;
+            Projectile.aiStyle = 0;          
+            Projectile.friendly = true;
+            Projectile.DamageType = DamageClass.Magic;
+            Projectile.penetrate = 2;        
+            Projectile.timeLeft = 60;        
+            Projectile.tileCollide = true;
+            Projectile.ignoreWater = false;
+        }
+
+        public override void AI()
+        {
+            // Give it a watery blue effect
+            Lighting.AddLight(Projectile.Center, 0f, 0.3f, 0.8f);
+
+            // Optional: move slightly forward like a wave
+            Projectile.position += new Vector2(Projectile.velocity.X * 0.5f, 0f);
+        }
+    }
+}
