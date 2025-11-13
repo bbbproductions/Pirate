40b76181173644d8dc974cf884646d12ddbc8a6b
bbbproductions <bbbproductions0@gmail.com>
2025-11-12 21:36:20 -0800

    hooray

--- Content/Items/TestSword.cs
+++ Content/Items/TestSword.cs
@@ -1,1 +1,34 @@
-//shit dont work
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Pirate.Content.Items
+{
+	public class Windblade : ModItem
+	{
+		public override void SetDefaults()
+		{
+			Item.damage = 40; // Lower damage than BeanSword
+			Item.DamageType = DamageClass.Melee;
+			Item.width = 40;
+			Item.height = 40;
+			Item.useTime = 20;
+			Item.useAnimation = 20;
+			Item.useStyle = ItemUseStyleID.Swing;
+			Item.knockBack = 10; // Increased knockback
+			Item.value = Item.buyPrice(silver: 1);
+			Item.rare = ItemRarityID.Blue;
+			Item.UseSound = SoundID.Item1;
+			Item.autoReuse = true;
+		}
+
+		public override void AddRecipes()
+		{
+			Recipe recipe = CreateRecipe();
+			recipe.AddIngredient(ItemID.Wood, 10); // Different recipe for flavor
+			recipe.AddIngredient(ItemID.Feather, 1); // Adds a thematic "wind" element
+			recipe.AddTile(TileID.WorkBenches);
+			recipe.Register();
+		}
+	}
+}
