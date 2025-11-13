9ff8ec55baf44b3936a9643e0bc571c1a2b941b3
bbbproductions <bbbproductions0@gmail.com>
2025-11-13 10:36:42 -0800

    added naval cutlass

---
+++ Content/Items/NavalCutlass.cs
@@ -0,0 +1,40 @@
+using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Pirate.Content.Items
+{
+    public class NavalCutlass : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.width = 46;
+            Item.height = 46;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.useTime = 20;
+            Item.useAnimation = 20;
+            Item.damage = 20;
+            Item.knockBack = 5.5f;
+            Item.scale = 1.1f;
+            Item.DamageType = DamageClass.Melee;
+            Item.value = Item.buyPrice(silver: 50);
+            Item.rare = ItemRarityID.Blue;
+            Item.UseSound = SoundID.Item1;
+        }
+
+    public class NavalCutlassGlobalNPC : GlobalNPC
+    {
+        public override void ModifyShop(NPCShop shop)
+        {
+            if (shop.NpcType == NPCID.Merchant)
+            {
+                shop.Add(new Item(ModContent.ItemType<NavalCutlass>())); 
+
+            }
+        }
+    }
+    }
+}
